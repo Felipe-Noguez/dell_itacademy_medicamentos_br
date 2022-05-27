e1b8e3b01536577b4065bd3e6ff1a57e49c38bcf
Felipe-Noguez <felipenoguez@outlook.com.br>
2022-05-26 22:59:25 -0300

    Criando projeto Medicamentos BR - Dell IT Academy

---
+++ src/main/java/com/itacademy/medicamentosbr/MedicamentosBrApplication.java
@@ -0,0 +1,13 @@
+package com.itacademy.medicamentosbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicamentosBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicamentosBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itacademy/medicamentosbr/MedicamentosBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itacademy.medicamentosbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicamentosBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
